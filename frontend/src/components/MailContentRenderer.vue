8938ef2b1f4bebee77420465d28b108076cf3d09
ColA <93503478+ColinKiiim@users.noreply.github.com>
2026-08-23 18:10:59 +0800

    style(mail): refine recipient copy button and remove heavy outer outline

--- frontend/src/components/MailContentRenderer.vue
+++ frontend/src/components/MailContentRenderer.vue
@@ -191,8 +191,8 @@ const handleSaveToS3 = async (filename, blob) => {
         <button type="button" class="mail-copy-button" :aria-label="t('copyRecipient')"
           :title="t('copyRecipient')" @click.stop="copyRecipientAddress">
           <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M8 8h11v11H8z" />
-            <path d="M5 16H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1" />
+            <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
           </svg>
         </button>
       </span>
